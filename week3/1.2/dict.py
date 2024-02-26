089fbace04d3c0c64d95a3e7d3c7ab22b8e17466
joren-dev <contactjorenx@gmail.com>
2024-02-26 16:10:17 +0100

    add: strucure

---
+++ week3/1.2/dict.py
@@ -0,0 +1,24 @@
+# Een lege dictionary aanmaken
+mijn_dict = {}
+
+# Gegevens toevoegen aan de dictionary
+mijn_dict['naam'] = 'John Doe'
+mijn_dict['leeftijd'] = 25
+mijn_dict['stad'] = 'Amsterdam'
+
+# Gegevens ophalen uit de dictionary
+print("Naam:", mijn_dict['naam'])
+print("Leeftijd:", mijn_dict['leeftijd'])
+print("Stad:", mijn_dict['stad'])
+
+# Gegevens wijzigen in de dictionary
+mijn_dict['leeftijd'] = 26
+
+# Gegevens opnieuw ophalen na wijziging
+print("Gewijzigde leeftijd:", mijn_dict['leeftijd'])
+
+# Controleren of een sleutel bestaat in de dictionary
+if 'gewicht' in mijn_dict:
+    print("Gewicht:", mijn_dict['gewicht'])
+else:
+    print("Gewicht is niet beschikbaar in de dictionary.")
