6932dd64f088b32315ad9b5f31375a0ff53217a5
joren-dev <contactjorenx@gmail.com>
2024-02-29 10:06:05 +0100

    deque: example algo

---
+++ week3/3.2/deque_window_sliding.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+def sliding_window_max(nums, k):
+    result = []  # Lijst om het maximale element in elk venster op te slaan
+    dq = deque()  # Deque om de indexen van de elementen in het huidige venster op te slaan
+
+    for i, num in enumerate(nums):
+        # Verwijder elementen die buiten het huidige venster vallen
+        while dq and dq[0] < i - k + 1:
+            dq.popleft()
+
+        # Verwijder alle elementen die kleiner zijn dan het huidige element
+        while dq and nums[dq[-1]] < num:
+            dq.pop()
+            
+        dq.append(i)
+
+        # Voeg het maximale element in het huidige venster toe aan het resultaat
+        if i >= k - 1:
+            result.append(nums[dq[0]])
+
+    return result
+
+# Voorbeeldgebruik
+nums = [4, 2, 1, 7, 8, 1, 2, 8, 10]
+k = 3
+# [4, 7, 8, 8, 8, 8, 10]
+result = sliding_window_max(nums, k)
+print(result)
