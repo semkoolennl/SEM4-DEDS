778634c7aa4827971968da16d68d638f85ea6cbc
joren-dev <contactjorenx@gmail.com>
2024-02-29 10:48:02 +0100

    fix: example

--- week3/3.2/examples/reverse_string.py
+++ week3/3.2/examples/reverse_string.py
@@ -1,13 +1,24 @@
-def reverse(s):
-    str = ""
-    for i in s:
-        str = i + str
-    return str
- 
-s = "EenSimpeleStringLol"
- 
-print("The original string is : ", end="")
-print(s)
- 
-print("The reversed string(using loops) is : ", end="")
-print(reverse(s))
+def omgekeerde_tekst(invoer_tekst):
+    # Converteer de string naar een lijst van karakters
+    karakter_lijst = list(invoer_tekst)
+    
+    # Gebruik twee pointers om de string ter plaatse om te keren
+    links, rechts = 0, len(karakter_lijst) - 1
+    while links < rechts:
+        # Wissel karakters bij de pointers links en rechts
+        karakter_lijst[links], karakter_lijst[rechts] = karakter_lijst[rechts], karakter_lijst[links]
+        
+        # Verplaats de pointers naar elkaar toe
+        links += 1
+        rechts -= 1
+    
+    # Converteer de lijst van karakters terug naar een string
+    omgekeerde_tekst = ''.join(karakter_lijst)
+    
+    return omgekeerde_tekst
+
+# Voorbeeldgebruik:
+invoer_tekst = "Hallo, Wereld!"
+omgekeerde_tekst = omgekeerde_tekst(invoer_tekst)
+print("Oorspronkelijke Tekst:", invoer_tekst)
+print("Omgekeerde Tekst:", omgekeerde_tekst)
