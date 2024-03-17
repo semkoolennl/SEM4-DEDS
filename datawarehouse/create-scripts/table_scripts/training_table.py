91ff19f1fc9c6a132f3f401d1fd1ec91310fadad
Sem Koolen <sem.koolen@gmail.com>
2024-03-17 04:03:04 +0100

    Added specific typing to result tables

--- datawarehouse/create-scripts/table_scripts/training_table.py
+++ datawarehouse/create-scripts/table_scripts/training_table.py
@@ -14,3 +14,6 @@ go_staff = load_db('source/go_staff.sqlite')
 
 training = load_table(go_staff, 'TRAINING')
 result = training.drop(columns=['YEAR'])
+
+result['SALES_STAFF_CODE'] = result['SALES_STAFF_CODE'].astype('int')
+result['COURSE_CODE'] = result['COURSE_CODE'].astype('int')
--- datawarehouse/create-scripts/test.py
+++ datawarehouse/create-scripts/test.py
@@ -4,7 +4,7 @@ from table_scripts.utils import *
 scripts = get_scripts()
 for script in scripts:
     print(f"Executing {script}...")
-    tablename = script.split('.')[0]
+    tablename = script.split('_table')[0]
     dataframe = execute_get_table(script)
-    print_columns(dataframe)
+    print(dataframe.dtypes)
     print()
