0be3cf3051e3b3385ed2073fd03f9403ea94ae71
Joren <contactjorenx@gmail.com>
2024-03-17 02:43:14 +0100

    fix: smth

--- datawarehouse/create-scripts/table_scripts/course_table.py
+++ datawarehouse/create-scripts/table_scripts/course_table.py
@@ -7,10 +7,22 @@ from utils import *
 # COURSE_COURSE_CODE
 # COURSE_COURSE_DESCRIPTION
 
-go_staff = load_db('source/go_staff.sqlite')
+import os
 
-course = load_table(go_staff, 'COURSE')
-result = course
-print(result)
+# Get the path of the current file
+current_file_path = os.path.abspath(__file__)
+
+# Get the current working directory
+current_directory = os.getcwd()
+
+# Compute the relative path
+relative_path = os.path.relpath(current_file_path, current_directory)
+print(relative_path)
+
+# go_staff = load_db('create-scripts/source/go_staff.sqlite')
+
+# course = load_table(go_staff, 'COURSE')
+# result = course
+# print(result)
 
 
